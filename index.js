88d28e3906cda8602115d00bddd08b29958b5274
muhammadumar0820 <salimjonovmuhammadumar372@gmail.com>
2023-12-23 18:46:21 +0300

    masalalar bajarildi

---
+++ index.js
@@ -0,0 +1,47 @@
+//Birinchi masala
+//function qiymat(arg){
+//    let arr = [1, 2, 3, 4];
+//    if (arr.indexOf(4)){
+//        return arr.indexOf(4)**2;
+//    }else{
+//        return "mavjud emas";
+//}
+
+//Ikkinchi masala
+//let arr1 = [1, 2, 3];
+//let arr2 = [4, 5, 6];
+//let arr3 = [7, 8, 9];
+//let res = arr2.concat(arr1, arr3);
+//console.log(res);
+
+//Uchinchi masala
+//    let arr = [1, 2, 3, 4, 5, 6];
+//    let a = arr.find((index) => {
+//    if (arr[3] == 4){
+//        console.log(arr[3] ** 2);
+//    }else{
+//        console.log("xatolik");
+//    }
+//    return index > 0;
+//});
+
+//To'rtinchi masala
+//let arr = [1, 2, 3, 4]
+//arr.forEach((value) =>{
+//    let counter = 0;
+//    if (arr[value] % 2 == 1){
+//        counter++;
+//    };
+//    return counter;
+//});
+
+//Beshinchi masala
+//let word = ["salom", "olma", "quyosh", "xayr"];
+//function words(arg){
+//    let res = [];
+//    for (let i = 0; i < arg.length; i++){
+//        res.push(arg[i].substr(length -1));
+//    }
+//    return res;
+//}
+//console.log(words(word));
